558bb2974717a407627b6d48148b70fd40405937
Jana Valenzuela <jnvlnzla@gmail.com>
2025-04-09 08:26:05 +0800

    Commit

--- app.py
+++ app.py
@@ -123,6 +123,7 @@ magnitude_colors = ["#FFE500", "#FFC400", "#FFA400", "#FF8300",
 app = dash.Dash(__name__,
                 external_stylesheets=["/content/drive/MyDrive/DLSU [2021-2025]/AY 2024-2025 TERM 2/DATA101/DATA101 Group 4/assets/style.css"],
                 suppress_callback_exceptions=True) # Important for dynamic layouts
+server = app.server
 
 min_year, max_year = 2000, datetime.now().year # Default fallbacks
 if 'Year_Earthquake' in magnitude_df.columns:
